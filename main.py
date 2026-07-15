96bc225ff0c9309a6fdf9d7cce680ed98af5b323
mhdhe919 <mhdhe919@student.liu.se>
2026-07-15 18:47:55 +0200

    Stage 1: root and health endpoints

--- main.py
+++ main.py
@@ -4,8 +4,23 @@ app = FastAPI()
 
 # Hello world endpoint from FastAPI documentation
 # To run server, use command fastapi dev
-# To see message in browser, visit http://localhost:8000
+# To see message in browser, visit http://localhost:8000/
 # To see message in terminal, run command curl -i http://localhost:8000/
-@app.get("/")
+"""@app.get("/")
 async def root():
     return {"message": "Hello World"}
+"""
+
+
+# First real endpoint for stage 1 of the assignment
+# To run server, use command fastapi dev
+# To see message in browser, visit http://localhost:8000/
+# To see message in terminal, run command curl -i http://localhost:8000/
+# Add /health at the end of the URL to see the enpoint's status
+@app.get("/")
+async def get_message():
+    return {"name": "Task API", "version": "1.0", "endpoints": ["/tasks"]}
+
+@app.get("/health")
+async def get_health():
+    return {"status": "ok"}
